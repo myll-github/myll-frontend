1ead36620b40a2c7ad6ef2d0d48f2c0c3d441c75
jongdae <asdfg9377@naver.com>
2023-06-28 01:07:41 +0900

    [Feat] 카카오 로그인 토큰 발급 확인

--- apps/web/src/pages/login/index.tsx
+++ apps/web/src/pages/login/index.tsx
@@ -1,21 +1,39 @@
 import { Button } from 'antd'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 
 import { Container, Label, Layout, Title } from './style'
 
-export const Login: React.FC = () => (
-  <Container>
-    <Layout>
-      <Title>
-        마일 여행자이시라면 <span>로그인</span> 해주세요
-      </Title>
-      <Button>초보 여행자에요</Button>
-      <Label>
-        SNS 계정 회원가입
-        <Button>카카오 로그인</Button>
-      </Label>
-    </Layout>
-  </Container>
-)
+export const Login: React.FC = () => {
+  const router = useRouter()
+
+  return (
+    <Container>
+      <Layout>
+        <Title>
+          마일 여행자이시라면 <span>로그인</span> 해주세요
+        </Title>
+        <Button>초보 여행자에요</Button>
+        <Label>
+          SNS 계정 회원가입
+          <Button
+            onClick={() =>
+              router.replace(
+                'https://kauth.kakao.com/oauth/authorize?client_id=' +
+                  'aa6a446ce59ce13350383bd1eee6eb4d' +
+                  '&redirect_uri=' +
+                  'https://localhost:3000/login' +
+                  '&response_type=code&' +
+                  'scope=profile_nickname age_range',
+              )
+            }
+          >
+            카카오 로그인
+          </Button>
+        </Label>
+      </Layout>
+    </Container>
+  )
+}
 
 export default Login
