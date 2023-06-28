0e8f3faa070cbba9e9a28b18874c1ecbbd099254
lodado <ycp998@naver.com>
2023-06-28 18:53:07 +0900

    feat: color 일부분 추가

--- packages/shared/src/color.ts
+++ packages/shared/src/color.ts
@@ -1,3 +1,21 @@
 const WHITE = '#fff'
 
-export { WHITE }
+const PRIMARY = {
+  BLUE: 'var(--primary-blue, #007AFF);',
+  SUB1_BLUE_LINEAR: 'var(--sub-blue-linear-1, linear-gradient(270deg, #007AFF 0%, #00BBE4 100%));',
+  SUB2_BLUE1: 'var(--sub-blue-1, #1890FF);',
+  SUB2_BLUE2: 'var(--sub-blue-2, #E3EFFF);',
+  SUB2_BLUE3: 'var(--sub-blue-3, #F0F5FF);',
+}
+
+const GRAY = {
+  _90: 'var(--gray-gray-90, #202020);',
+  _80: 'var(--gray-gray-80, #39434F);',
+  _70: 'var(--gray-gray-70, #848E9B);',
+  _50: 'var(--gray-gray-50, #C9D0DE);',
+  _30: 'var(--gray-gray-30, #E7EAF1);',
+  _20: 'var(--gray-gray-20, #F4F5F8);',
+  WHITE: 'var(--gray-white, #FFF)',
+}
+
+export { GRAY,PRIMARY }
