04bae82c79bde1f3b4951cf495b81425c0089aa2
jongdae <asdfg9377@naver.com>
2023-06-28 19:13:04 +0900

    [Feat] dotenv 설치

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,8 +2,13 @@
 const withPWA = require('next-pwa')({
   dest: './public',
 })
+require('dotenv').config()
 
 module.exports = withPWA({
+  env: {
+    KAKAO_CLIENT_KEY: process.env.KAKAO_CLIENT_KEY,
+    KAKAO_LOGIN_REDIRECT_URL: process.env.KAKAO_LOGIN_REDIRECT_URL,
+  },
   reactStrictMode: true,
   transpilePackages: ['ui'],
 
