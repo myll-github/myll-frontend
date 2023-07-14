43011cdd06573b912ffeb1db9db07ba5cb04d3be
lodado <ycp998@naver.com>
2023-07-14 21:00:09 +0900

    chore: storybook 설치

--- packages/myll-ui/src/Button.tsx
+++ packages/myll-ui/src/Button.tsx
@@ -1,5 +1,28 @@
 import { Button as AntdButton } from 'antd'
 
+interface ButtonProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string
+  /**
+   * How large should the button be?
+   */
+  size?: 'small' | 'medium' | 'large'
+  /**
+   * Button contents
+   */
+  label: string
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void
+}
+
 const Button = () => {
   return <AntdButton className="w-[500px] h-[500px] bg-slate-500">123</AntdButton>
 }
