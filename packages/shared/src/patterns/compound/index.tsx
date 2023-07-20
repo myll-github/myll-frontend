fa004f72bab067c2029b414180f870702330ecae
lodado <ycp998@naver.com>
2023-07-20 19:48:22 +0900

    feat: compound pattern template 작성

---
+++ packages/shared/src/patterns/compound/index.tsx
@@ -0,0 +1,24 @@
+import { createContext, ReactNode, useContext, useMemo } from 'react'
+
+const CompoundContext = createContext(null as unknown)
+
+interface CompoundProps {
+  children: ReactNode
+  providerValues: unknown[]
+}
+
+export const CompoundProvider = ({ children, ...providerValue }: CompoundProps) => {
+  const memoProviderValue = useMemo(() => ({ ...providerValue }), [providerValue])
+
+  return <CompoundContext.Provider value={memoProviderValue}>{children}</CompoundContext.Provider>
+}
+
+export const useCompound = () => {
+  const context = useContext(CompoundContext)
+
+  if (!context) {
+    throw new Error('no Context provider!')
+  }
+
+  return context
+}
