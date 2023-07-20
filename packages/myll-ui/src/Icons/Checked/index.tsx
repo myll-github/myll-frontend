62565f835d867b5937660d56b021fb370ca1b40b
lodado <ycp998@naver.com>
2023-07-20 20:52:35 +0900

    feat: checkbox icon 생성

---
+++ packages/myll-ui/src/Icons/Checked/index.tsx
@@ -0,0 +1,6 @@
+import AntdCheckCircleFilled from '@ant-design/icons/lib/icons/CheckCircleFilled'
+
+const CheckCircleFilled = () => {
+  return <AntdCheckCircleFilled className="" />
+}
+export default CheckCircleFilled
