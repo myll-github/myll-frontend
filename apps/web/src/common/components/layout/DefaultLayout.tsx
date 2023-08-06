308195600b470a2b9c52153f0ecb46863fc85db5
lodado <ycp998@naver.com>
2023-08-06 12:02:02 +0900

    feat: layout 중앙정렬

--- apps/web/src/common/components/layout/DefaultLayout.tsx
+++ apps/web/src/common/components/layout/DefaultLayout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react'
 
 const DefaultLayout = ({ children }: { children: ReactNode }) => {
-  return <div className="relative w-screen h-screen md:w-[768px]">{children}</div>
+  return <div className="relative left-1/2 -translate-x-1/2 w-screen h-screen md:w-[768px]">{children}</div>
 }
 
 export default DefaultLayout
