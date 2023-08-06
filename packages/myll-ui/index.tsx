dab52b3f76ca680fc4f79ce6421fa7139ba45925
jongdae <asdfg9377@naver.com>
2023-08-07 00:18:14 +0900

    feat: Antd Divider 컴포넌트 추가

--- packages/myll-ui/index.tsx
+++ packages/myll-ui/index.tsx
@@ -1,7 +1,9 @@
+import { Divider } from 'antd'
+
 import Button from './src/Components/Button/Button'
 import Input from './src/Components/Input'
 
 export * from './src/Components/Card'
 export * from './src/Header'
 
-export { Button, Input }
+export { Button, Divider, Input }
