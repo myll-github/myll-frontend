2b488da4a095467168746190864b859ba5548a7c
lodado <ycp998@naver.com>
2023-08-14 23:01:16 +0900

    chore: card css 수정

--- packages/myll-ui/src/Components/Card/HorizontalCard.tsx
+++ packages/myll-ui/src/Components/Card/HorizontalCard.tsx
@@ -10,7 +10,7 @@ const HorizontalCard: CompoundCard<BasicCardProps> = ({ children, isSelected, on
   return (
     <Card
       onClick={onClick}
-      className={`${BG_COLOR} w-320pxr h-115pxr gap-3pxr flex p-7pxr items-center rounded-lg border border-solid`}
+      className={`${BG_COLOR} w-320pxr h-115pxr flex p-10pxr items-center rounded-lg border border-solid`}
     >
       {children}
     </Card>
@@ -18,14 +18,14 @@ const HorizontalCard: CompoundCard<BasicCardProps> = ({ children, isSelected, on
 }
 
 HorizontalCard.CardImage = ({ src, alt }: ImageProps) => {
-  return <CardImage src={src} alt={alt} className="relative object-fill rounded-sm w-120pxr h-90pxr mr-27pxr" />
+  return <CardImage src={src} alt={alt} className="relative object-fill rounded-sm w-120pxr h-90pxr mx-10pxr" />
 }
 
 const Description = ({ mainTitle, subTitle }: DescriptionProps) => {
   return (
     <div className="flex flex-col w-150pxr h-72pxr mt-15pxr">
-      <SubTitle title={subTitle} className="h-50pxr flex flex-col justify-end" />
-      <MainTitle className="-mt-2pxr" title={mainTitle} />
+      <SubTitle title={subTitle} className="flex flex-col justify-end h-50pxr" />
+      <MainTitle className="" title={mainTitle} />
     </div>
   )
 }
