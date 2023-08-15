7e9cf9c8a2c70b0baeb070a032b063272b8f8aa1
lodado <ycp998@naver.com>
2023-08-15 17:09:17 +0900

    feat: horizontal card에 icon 추가

--- apps/docs/src/design-system/Card.stories.tsx
+++ apps/docs/src/design-system/Card.stories.tsx
@@ -20,55 +20,6 @@ const ExampleData = [
     alt: '1',
     url: 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',
   },
-  {
-    id: 2,
-    subTitle: 'apple2',
-    mainTitle: 'apple2',
-    alt: '1',
-    url: 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',
-  },
-  {
-    id: 3,
-    subTitle: 'apple3',
-    mainTitle: 'apple3',
-    alt: '1',
-    url: 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',
-  },
-  {
-    id: 4,
-    subTitle: 'apple4',
-    mainTitle: 'apple4',
-    alt: '4',
-    url: 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',
-  },
-  {
-    id: 5,
-    subTitle: 'apple5',
-    mainTitle: 'apple5',
-    alt: '5',
-    url: 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',
-  },
-  {
-    id: 6,
-    subTitle: 'apple6',
-    mainTitle: 'apple6',
-    alt: '6',
-    url: 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',
-  },
-  {
-    id: 7,
-    subTitle: 'apple7',
-    mainTitle: 'apple7',
-    alt: '7',
-    url: 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',
-  },
-  {
-    id: 8,
-    subTitle: 'apple8',
-    mainTitle: 'apple8',
-    alt: '8',
-    url: 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',
-  },
 ]
 
 export default meta
--- packages/myll-ui/src/Components/Card/HorizontalCard.tsx
+++ packages/myll-ui/src/Components/Card/HorizontalCard.tsx
@@ -1,4 +1,5 @@
 import { FC, MouseEventHandler, ReactComponentElement, ReactNode, useId } from 'react'
+import { ICON_CHECKED, ICON_DEFAULT } from 'shared'
 
 import Card from './components/base'
 import { CardImage, ImageProps, MainTitle, SubTitle } from './components/compound'
@@ -10,9 +11,12 @@ const HorizontalCard: CompoundCard<BasicCardProps> = ({ children, isSelected, on
   return (
     <Card
       onClick={onClick}
-      className={`${BG_COLOR} w-320pxr h-115pxr flex p-10pxr items-center rounded-lg border border-solid`}
+      className={`${BG_COLOR} relative w-320pxr h-115pxr flex p-10pxr items-center rounded-lg border border-solid`}
     >
       {children}
+      <div className="absolute top-2.5 right-2.5">
+        {isSelected ? <ICON_CHECKED className="text-PRIMARY_BLUE" /> : <ICON_DEFAULT className="text-GRAY_60" />}
+      </div>
     </Card>
   )
 }
