df11aa512101c8c1c0fc497344db8bb30eb38e4e
jongdae <asdfg9377@naver.com>
2023-08-15 13:42:29 +0900

    feat: 인증번호 필드 추가

--- apps/web/src/pages/login/sign-up/index.page.tsx
+++ apps/web/src/pages/login/sign-up/index.page.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable turbo/no-undeclared-env-vars */
-import { Button, Divider, Input } from 'myll-ui'
+import { Button, Divider, Input, Space } from 'myll-ui'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -16,6 +16,17 @@ export const SignUp = () => {
         <div className="flex flex-col gap-20pxr">
           <Input label="사용자 이름" size="large" placeholder="한/영 15자 이내" />
           <Input label="이메일" size="large" placeholder="이메일을 입력해주세요" inputMode="email" />
+          <Space.Compact block size="large" className="w-full flex justify-center items-center">
+            <Input size="large" placeholder="인증번호" addonBefore={<div className="INPUT-LABEL2">인증코드</div>} />
+            <Button
+              className="border-1 border-l-0 border-solid border-GRAY_40 text-PRIMARY_BLUE BUTTON-MEDIUM"
+              type="button"
+              variant="medium"
+              color="text"
+            >
+              재전송
+            </Button>
+          </Space.Compact>
           <Input label="비밀번호" size="large" placeholder="8자 이상 비밀번호" inputType="password" />
         </div>
         <div className="flex flex-col mt-auto mb-60pxr">
