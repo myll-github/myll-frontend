a881a3cbc181dc9088cfbf4ceacd474ed7bd4f25
jongdae <asdfg9377@naver.com>
2023-08-22 19:44:11 +0900

    feat: 회원가입 login 기능 구현

--- apps/web/src/pages/login/sign-up/index.page.tsx
+++ apps/web/src/pages/login/sign-up/index.page.tsx
@@ -1,16 +1,21 @@
 /* eslint-disable turbo/no-undeclared-env-vars */
-import { Button, Divider, Input, Space } from 'myll-ui'
-import Image from 'next/image'
-import Link from 'next/link'
+import { Button, Input, Space } from 'myll-ui'
 import { useEffect, useState } from 'react'
 
-import DefaultLayout from '@/common/components/layout/DefaultLayout'
+import { SendEmail } from '@/common/api/send-email/SendEmail'
+import { UserSignUp } from '@/common/api/signup/UserSignUp'
+import { Verify } from '@/common/api/verify/Verify'
+import DefaultLayout from '@/common/components/Layout/DefaultLayout'
 
 export const SignUp = () => {
   const [openValidation, setOpenValidation] = useState<boolean>(false)
 
   const [name, setName] = useState<string>('')
   const [email, setEmail] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [emailCode, setEmailCode] = useState<string>('')
+
+  const [isEmailValidated, setIsEmailValidated] = useState<boolean>(false)
 
   useEffect(() => {
     const kakaoName = sessionStorage.getItem('nickname')
@@ -20,11 +25,34 @@ export const SignUp = () => {
     if (kakaoEmail !== 'null') setEmail(kakaoEmail)
   }, [])
 
+  useEffect(() => {
+    if (emailCode.length === 6) {
+      Verify(email, emailCode)
+        .then(() => {
+          setIsEmailValidated(true)
+        })
+        .catch(console.log)
+    }
+  }, [emailCode])
+
+  const sendEmail = async () => {
+    try {
+      await SendEmail(email)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   // @TODO debounce, useCallback 적용 및 text state 관리 로직 추가
   const handleValidateUsername = (text: string) => {
     if (text.length <= 15 && !/[^a-zA-Z가-힣]/.test(text)) return true
     return false
   }
+
+  const handleClickSignUp = async () => {
+    await UserSignUp(email, password, name)
+  }
+
   return (
     <DefaultLayout>
       <div className="w-full h-full flex flex-col justify-center pl-30pxr pr-30pxr">
@@ -50,28 +78,53 @@ export const SignUp = () => {
             inputMode="email"
             onChange={(e) => setEmail(e.target.value)}
             addonAfter={
-              <Button type="button" variant="small" color="text" onClick={() => setOpenValidation(true)}>
+              <Button
+                type="button"
+                variant="small"
+                color="text"
+                onClick={() => {
+                  setOpenValidation(true)
+                  sendEmail()
+                }}
+              >
                 인증
               </Button>
             }
           />
           {openValidation && (
             <Space.Compact block size="large" className="w-full flex justify-center items-center">
-              <Input size="large" placeholder="인증번호" addonBefore={<div className="INPUT-LABEL2">인증코드</div>} />
+              <Input
+                size="large"
+                placeholder="인증번호"
+                value={emailCode}
+                disabled={isEmailValidated}
+                onChange={(e) => {
+                  if (e.target.value.length <= 6) setEmailCode(e.target.value)
+                }}
+                addonBefore={<div className="INPUT-LABEL2">인증코드</div>}
+              />
               <Button
                 className="border-1 border-l-0 border-solid border-GRAY_40 text-PRIMARY_BLUE BUTTON-MEDIUM"
                 type="button"
                 variant="medium"
                 color="text"
+                disabled={isEmailValidated}
               >
                 재전송
               </Button>
             </Space.Compact>
           )}
-          <Input label="비밀번호" size="large" placeholder="8자 이상 비밀번호" inputType="password" />
+          <Input
+            label="비밀번호"
+            size="large"
+            placeholder="8자 이상 비밀번호"
+            inputType="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <div className="flex flex-col mt-auto mb-60pxr">
-          <Button type="button" variant="medium" color="primary">
+          <Button type="button" variant="medium" color="primary" onClick={handleClickSignUp}>
             회원가입
           </Button>
         </div>
