64f8f85fb154abcae7d8dc1f377264fd61877a79
lodado <ycp998@naver.com>
2023-09-05 20:42:58 +0900

    feat: next/link로 local 페이지와 local/attraction 연결

--- apps/web/src/pages/local/section/LocalPlaceSection.tsx
+++ apps/web/src/pages/local/section/LocalPlaceSection.tsx
@@ -1,4 +1,5 @@
 import { Button } from 'myll-ui'
+import Link from 'next/link'
 
 import LocalAttractionMenuList from '../components/LocalAttractionMenuList'
 
@@ -7,9 +8,12 @@ const LocalPlaceSection = () => {
     <section className="">
       <div className="flex flex-row items-center justify-between w-full border-b border-solid pb-12pxr mb-30pxr HEADER-H5 border-GRAY_30 text-GRAY_100 px-20pxr">
         <h2 className="text-center SUBTITLE-T2">새로 등록된 관광지</h2>
-        <Button color="text" onClick={() => {}} type="button" variant="small">
-          전체보기
-        </Button>
+
+        <Link href="/local/attraction">
+          <Button color="text" onClick={() => {}} type="button" variant="small">
+            전체보기
+          </Button>
+        </Link>
       </div>
 
       <LocalAttractionMenuList />
