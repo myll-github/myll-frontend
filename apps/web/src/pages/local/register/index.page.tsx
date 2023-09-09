a6b11bb836860b692c21b4d7372a2639aa654279
lodado <ycp998@naver.com>
2023-09-09 16:21:20 +0900

    feat: local 글쓰기 버튼 구현

--- apps/web/src/pages/local/register/index.page.tsx
+++ apps/web/src/pages/local/register/index.page.tsx
@@ -1,30 +1,15 @@
 import { dehydrate, QueryClient } from '@tanstack/react-query'
 import { Alert, AppBar, Button, Tab } from 'myll-ui'
-import Link from 'next/link'
 import { ICON_EDIT } from 'shared'
 
 import NavLayout from '@/common/components/Layout/NavLayout'
 
-import LocalHeader from '../section/LocalHeader'
-import LocalPlaceSection from './section/LocalPlaceSection'
-
-export const Home = () => {
+export const Register = () => {
   return (
     <>
-      <LocalHeader />
       <NavLayout>
-        <main className="relative flex flex-col gap-10">
-          <LocalPlaceSection />
-        </main>
+        <main className="relative flex flex-col gap-10">123</main>
       </NavLayout>
-
-      <div className="fixed left-1/2 -translate-x-1/2 bottom-118pxr flex flex-row justify-end w-screen md:w-[768px] max-h-98px px-20pxr transform">
-        <Link href="/local/register">
-          <Button type="button" onClick={() => {}} variant="medium" icon={<ICON_EDIT />}>
-            글쓰기
-          </Button>
-        </Link>
-      </div>
     </>
   )
 }
@@ -54,4 +39,4 @@ export const getServerSideProps = async () => {
 
 */
 
-export default Home
+export default Register
