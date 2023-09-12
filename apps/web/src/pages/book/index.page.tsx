58bed7913e3643e2ab7a6649afc835ce74707c52
jongdae <asdfg9377@naver.com>
2023-09-13 00:07:52 +0900

    feat: 일정 등록 페이지

--- apps/web/src/pages/book/index.page.tsx
+++ apps/web/src/pages/book/index.page.tsx
@@ -1,11 +1,65 @@
+import { Button, DatePicker, Input, Modal } from 'myll-ui'
+import { useState } from 'react'
+
 import NavLayout from '@/common/components/Layout/NavLayout'
 
-export const Login = () => {
-  return (
+import { AddPlanPage } from './add/AddPlanPage'
+import PlanBodyAdd from './section/PlanBodyAdd'
+import PlanHeader from './section/PlanHeader'
+import SimpleList from './section/SimpleList'
+
+export const Book = () => {
+  // 일정 등록
+  const [openRangePicker, setOpenRangePicker] = useState<boolean>(false)
+  const [addDates, setAddDates] = useState<string[]>([])
+  const [title, setTitle] = useState<string>('')
+
+  const [addMode, setAddMode] = useState<boolean>(false)
+  return addMode ? (
+    <AddPlanPage
+      title={title}
+      startDate={addDates[0]}
+      endDate={addDates[1]}
+      selectedDatas={[]}
+      onBack={() => setAddMode(false)}
+    />
+  ) : (
     <NavLayout>
-      <main>123</main>
+      <PlanHeader />
+
+      <PlanBodyAdd openModal={() => setOpenRangePicker(true)} />
+
+      <SimpleList onChangeMode={() => {}} />
+
+      <Modal
+        open={openRangePicker}
+        onCancel={() => setOpenRangePicker(false)}
+        cancelButtonText="취소"
+        submitButtonText="일정 등록"
+        onSubmit={() => {
+          setAddMode(true)
+          setOpenRangePicker(false)
+        }}
+      >
+        <div className="h-150pxr flex flex-col items-center gap-20pxr">
+          <div className="w-full">
+            <Input
+              label="여행 정보"
+              placeholder="여행 제목을 입력해주세요."
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <DatePicker.RangePicker
+            format="YYYY년 M월 D일"
+            className="w-full"
+            allowClear={false}
+            onChange={(_, values) => setAddDates(values)}
+          />
+        </div>
+      </Modal>
     </NavLayout>
   )
 }
 
-export default Login
+export default Book
