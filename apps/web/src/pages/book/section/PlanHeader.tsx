9feef6dbc8e216127ac64a8b0cc5431013dc6bb6
jongdae <asdfg9377@naver.com>
2023-09-17 20:58:17 +0900

    fix: AppBar 컴포넌트 사용하도록 변경

--- apps/web/src/pages/book/section/PlanHeader.tsx
+++ apps/web/src/pages/book/section/PlanHeader.tsx
@@ -1,13 +1,21 @@
+import { AppBar } from 'myll-ui'
 import { ICON_CHAT, ICON_NOTIFICATION } from 'shared'
 
 const PlanHeader = () => (
-  <div className="h-76pxr pt-30pxr flex justify-between">
-    <span className="h-24pxr ml-72pxr text-center flex flex-grow SUBTITLE-T3 justify-center items-center">Book</span>
-    <span className="flex h-24pxr mr-20pxr gap-12pxr items-center">
-      <ICON_NOTIFICATION />
-      <ICON_CHAT />
-    </span>
-  </div>
+  <header>
+    <AppBar>
+      <AppBar.LeftSection>
+        <AppBar.FallbackButton />
+      </AppBar.LeftSection>
+      <AppBar.MiddleSection>Book</AppBar.MiddleSection>
+      <AppBar.RightSection>
+        <span className="flex h-full gap-10pxr items-center">
+          <ICON_NOTIFICATION />
+          <ICON_CHAT />
+        </span>
+      </AppBar.RightSection>
+    </AppBar>
+  </header>
 )
 
 export default PlanHeader
