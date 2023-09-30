48e14d2c9e99792864914808a3ae43bfb0afd8cd
jongdae <asdfg9377@naver.com>
2023-09-30 22:10:47 +0900

    feat: email 필드 추가

--- apps/web/src/stores/useTokenStore/index.ts
+++ apps/web/src/stores/useTokenStore/index.ts
@@ -1,22 +1,30 @@
-import { ItemType } from 'myll-ui/src/Components/MenuList/type'
 import { create } from 'zustand'
 import { immer } from 'zustand/middleware/immer'
 
 interface State {
   accessToken: string
+  email: string
 }
 
 interface Actions {
   setAccessToken: (token: string) => void
+  setEmail: (email: string) => void
 }
 
 const useTokenStore = create(
   immer<State & Actions>((set) => ({
     accessToken: '',
+    email: '',
 
     setAccessToken: (token: string) => {
       set((state) => {
         state.accessToken = token
+      })
+    },
+
+    setEmail: (email: string) => {
+      set((state) => {
+        state.email = email
       })
     },
   })),
