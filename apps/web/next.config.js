5645e77b58462799e57a38cf52418a68ebeecee7
jongdae <asdfg9377@naver.com>
2023-10-10 21:55:05 +0900

    fix: source 변경

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -51,7 +51,7 @@ module.exports = withPWA({
     return [
       {
         // matching all API routes
-        source: '/*',
+        source: '/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
