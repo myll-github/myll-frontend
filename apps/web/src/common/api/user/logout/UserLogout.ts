8bb515d3686a3f5c010bb66009cda43ddf0815b8
jongdae <asdfg9377@naver.com>
2023-10-14 20:57:00 +0900

    fix: api 파일 이동

--- apps/web/src/common/api/user/logout/UserLogout.ts
+++ apps/web/src/common/api/user/logout/UserLogout.ts
@@ -1,4 +1,4 @@
-import { authAPI } from '..'
+import { authAPI } from '../..'
 
 export const UserLogout = async () => {
   const response = await authAPI({ method: 'DELETE', url: `${process.env.NEXT_PUBLIC_API_URL}/logout` })
