a60cb78d0b0eb97b3d74acfa6971056d2da0518a
jongdae <asdfg9377@naver.com>
2023-10-14 20:07:59 +0900

    feat: login 후 redirect 로직 추가

--- apps/web/src/pages/login/index.page.tsx
+++ apps/web/src/pages/login/index.page.tsx
@@ -35,75 +35,81 @@ export const Login = (props: LoginProps) => {
   const handleEmailLogin = async () => {
     try {
       const response = await UserLogin(email, password)
-      nookies.set(undefined, 'accessToken', response.data.accessToken)
-      nookies.set(undefined, 'refreshToken', response.data.refreshToken)
-      nookies.set(undefined, 'userEmail', email)
-      router.replace('/home')
+
+      const { accessToken, refreshToken, isSetPreference } = response.data
+      nookies.set(undefined, 'accessToken', accessToken, { maxAge: 1800 })
+      nookies.set(undefined, 'refreshToken', refreshToken, { maxAge: 10800 })
+      nookies.set(undefined, 'userEmail', email, { maxAge: 10800 })
+
+      if (isSetPreference) router.replace('/home')
+      else router.replace('/recommend')
     } catch (error) {
       setOpenAlert({ isVisible: true, type: 'error', message: error.message })
     }
   }
 
   return (
-    <DefaultLayout>
-      <div className="flex flex-col w-full h-screen pl-30pxr pr-30pxr">
-        <div className="text-center HEADER-H1 break-keep mb-30pxr">
-          마일 여행자이시라면 <span className="text-PRIMARY_BLUE">로그인</span> 해주세요
-        </div>
-
-        <Input
-          size="large"
-          value={email}
-          placeholder="이메일을 입력해주세요"
-          inputMode="email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Input
-          size="large"
-          value={password}
-          placeholder="비밀번호를 입력해주세요"
-          inputType="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-
-        <div className="flex flex-col gap-10pxr mt-30pxr">
-          <Button variant="large" color="primary" type="button" onClick={handleEmailLogin}>
-            로그인
-          </Button>
-          <div className="flex items-center justify-center w-full h-30pxr">
-            <Button color="text" variant="medium" type="button">
-              이메일 찾기
-            </Button>
-            <Divider type="vertical" />
-            <Button color="text" variant="medium" type="button">
-              비밀번호 찾기
-            </Button>
+    <>
+      <DefaultLayout>
+        <main className="flex flex-col w-full h-screen pl-30pxr pr-30pxr">
+          <div className="text-center HEADER-H1 break-keep mb-30pxr">
+            마일 여행자이시라면 <span className="text-PRIMARY_BLUE">로그인</span> 해주세요
           </div>
-        </div>
 
-        <div className="flex flex-col mt-auto mb-0 text-left gap-10pxr INPUT-LABEL2 text-GRAY_70">
-          SNS 계정 회원가입
-          <div className="flex justify-center w-full cursor-pointer">
-            <Link
-              href={
-                `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.KAKAO_CLIENT_KEY}&redirect_uri=${process.env.KAKAO_LOGIN_REDIRECT_URL}&response_type=code&` +
-                `scope=profile_nickname age_range`
-              }
-            >
-              <Image width={265} height={40} src="/kakao_start.png" alt="no img" />
-            </Link>
-          </div>
-          <Alert
-            isVisible={openAlert.isVisible}
-            onVisibleChange={(flag) => {
-              if (!flag) setOpenAlert({ isVisible: false, message: '' })
-            }}
-            type={openAlert.type}
-            message={openAlert.message}
+          <Input
+            size="large"
+            value={email}
+            placeholder="이메일을 입력해주세요"
+            inputMode="email"
+            onChange={(e) => setEmail(e.target.value)}
           />
-        </div>
-      </div>
-    </DefaultLayout>
+          <Input
+            size="large"
+            value={password}
+            placeholder="비밀번호를 입력해주세요"
+            inputType="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <div className="flex flex-col gap-10pxr mt-30pxr">
+            <Button variant="large" color="primary" type="button" onClick={handleEmailLogin}>
+              로그인
+            </Button>
+            <div className="flex items-center justify-center w-full h-30pxr">
+              <Button color="text" variant="medium" type="button">
+                이메일 찾기
+              </Button>
+              <Divider type="vertical" />
+              <Button color="text" variant="medium" type="button">
+                비밀번호 찾기
+              </Button>
+            </div>
+          </div>
+
+          <div className="flex flex-col mt-auto mb-0 text-left gap-10pxr INPUT-LABEL2 text-GRAY_70">
+            SNS 계정 회원가입
+            <div className="flex justify-center w-full cursor-pointer">
+              <Link
+                href={
+                  `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.KAKAO_CLIENT_KEY}&redirect_uri=${process.env.KAKAO_LOGIN_REDIRECT_URL}&response_type=code&` +
+                  `scope=profile_nickname age_range`
+                }
+              >
+                <Image width={265} height={40} src="/kakao_start.png" alt="no img" />
+              </Link>
+            </div>
+            <Alert
+              isVisible={openAlert.isVisible}
+              onVisibleChange={(flag) => {
+                if (!flag) setOpenAlert({ isVisible: false, message: '' })
+              }}
+              type={openAlert.type}
+              message={openAlert.message}
+            />
+          </div>
+        </main>
+      </DefaultLayout>
+    </>
   )
 }
 
