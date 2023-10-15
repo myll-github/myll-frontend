24707368ef3d9c0d3b825f8315f8bc400d1bae06
jongdae <asdfg9377@naver.com>
2023-10-16 02:17:08 +0900

    fix: icon 주석 해제

--- apps/web/src/pages/login/kakao-btn/KaKaoStartButton.tsx
+++ apps/web/src/pages/login/kakao-btn/KaKaoStartButton.tsx
@@ -1,4 +1,5 @@
 import { Button } from 'myll-ui'
+import { ICON_KAKAO_CHAT } from 'shared'
 
 const KaKaoStartButton = () => {
   return (
@@ -8,7 +9,7 @@ const KaKaoStartButton = () => {
       variant="large"
       color="outlined"
     >
-      {/* <ICON_KAKAO_CHAT width={18} height={18} /> */}
+      <ICON_KAKAO_CHAT width={18} height={18} />
       카카오로 시작하기
     </Button>
   )
