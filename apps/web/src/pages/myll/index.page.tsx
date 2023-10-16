a13b1ffc70b9980d54aafe4d608b24bafbec2559
jongdae <asdfg9377@naver.com>
2023-10-17 01:20:05 +0900

    feat: Info Drawer 추가

--- apps/web/src/pages/myll/index.page.tsx
+++ apps/web/src/pages/myll/index.page.tsx
@@ -1,8 +1,8 @@
 import { dehydrate, QueryClient } from '@tanstack/react-query'
-import { Avatar, Button, Input, Modal } from 'myll-ui'
+import { Avatar, Button, Input, Modal, Tab } from 'myll-ui'
 import { GetServerSidePropsContext } from 'next'
 import nookies from 'nookies'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { ICON_PROFILE_14 } from 'shared'
 
 import { getCookieHeader, withAuth } from '@/common/api'
@@ -10,86 +10,72 @@ import { userInfoFn, userInfoKey, useUserInfo } from '@/common/api/user/info'
 import { UserUpdate } from '@/common/api/user/update/UserUpdate'
 import NavLayout from '@/common/components/Layout/NavLayout'
 
+import InfoDrawer from './info-drawer/InfoDrawer'
 import MyllHeader from './section/MyllHeader'
 
 export const Myll = () => {
   const { data } = useUserInfo()
-  const { email, nickname, introduction } = data.dataValues
-
-  const [value, setValue] = useState<{ nickname: string; email: string; introduction: string }>({
-    nickname,
-    email,
-    introduction,
-  })
 
   const [open, setOpen] = useState<boolean>(false)
-
-  const handleUserInfoUpdate = async (userInfo: { nickname?: string; introduction?: string }) => {
-    await UserUpdate(userInfo)
-  }
 
   return (
     <NavLayout>
       <MyllHeader />
-      <main className="pl-30pxr pr-30pxr">
-        <div className="w-full text-center mt-20pxr">
+      <main>
+        <div className="w-full text-center mt-10pxr mb-10pxr">
           <Avatar className="bg-white" size={100} icon={<ICON_PROFILE_14 className="w-full h-full" />} />
         </div>
-        <div className="mt-96pxr w-full flex flex-col gap-32pxr items-center">
-          <div className="w-full flex">
-            <Input
-              theme="bottom-border"
-              label="프로필 이름"
-              value={value.nickname}
-              onChange={(e) => setValue((prev) => ({ ...prev, nickname: e.target.value }))}
-            />
-            <Button
-              className="mt-auto"
-              color="text"
-              type="button"
-              variant="medium"
-              onClick={() => handleUserInfoUpdate({ nickname: value.nickname })}
-            >
-              변경
-            </Button>
+        {open || (
+          <div className="flex flex-col items-center justify-center gap-10pxr mb-20pxr">
+            <div className="SUBTITLE-T3 text-GRAY_80">{data?.dataValues?.nickname || ''}</div>
+            <div className="SUBTITLE-T8 text-GRAY_70">{data?.dataValues?.email || ''}</div>
           </div>
-          <div className="w-full flex">
-            <Input theme="bottom-border" label="이메일" value={value.email} />
-            <Button className="mt-auto" color="text" type="button" variant="medium" disabled>
-              변경
-            </Button>
-          </div>
-          <div className="w-full flex">
-            <Input
-              theme="bottom-border"
-              label="소개"
-              value={value.introduction}
-              onChange={(e) => setValue((prev) => ({ ...prev, introduction: e.target.value }))}
-            />
-            <Button
-              className="mt-auto"
-              color="text"
-              type="button"
-              variant="medium"
-              onClick={() => handleUserInfoUpdate({ introduction: value.introduction })}
-            >
-              변경
-            </Button>
-          </div>
-          <Button className="mt-30pxr opacity-40" type="button" variant="block" onClick={() => setOpen(true)}>
-            인증하기
+        )}
+        <div className="w-full flex justify-center mb-10pxr">
+          <Button
+            className={`${open && 'opacity-40'}`}
+            type="button"
+            variant="small"
+            color="text"
+            onClick={() => setOpen(true)}
+          >
+            {open ? '이미지 편집' : '프로필 편집'}
           </Button>
         </div>
+        <InfoDrawer
+          open={open}
+          onClose={async () => {
+            setOpen(false)
+            // @TODO refetch 사용하도록 변경 필요
+            window.location.reload()
+          }}
+        />
+        <Tab
+          size="large"
+          tabBarGutter={60}
+          className="w-full"
+          centered
+          defaultActiveKey="마이 트립"
+          items={[
+            {
+              children: <div className="w-full">test1</div>,
+              key: '마이 트립',
+              label: '마이 트립',
+            },
+            {
+              children: <div className="w-full">test2</div>,
+              key: '찜한 여행지',
+              label: '찜한 여행지',
+            },
+            {
+              children: <div className="w-full">test3</div>,
+              key: '작성한 글',
+              label: '작성한 글',
+            },
+          ]}
+          onChange={() => {}}
+        />
       </main>
-      <Modal
-        open={open}
-        submitButtonText="확인"
-        onSubmit={() => setOpen(false)}
-        cancelButtonText="취소"
-        onCancel={() => setOpen(false)}
-      >
-        <div>준비중인 기능입니다.</div>
-      </Modal>
     </NavLayout>
   )
 }
@@ -98,17 +84,24 @@ export const getServerSideProps = withAuth(async (context: GetServerSidePropsCon
   const queryClient = new QueryClient()
   const initHeaders = getCookieHeader(context)
 
-  await queryClient.prefetchQuery({
-    queryKey: userInfoKey(),
-    queryFn: userInfoFn({ initHeaders }, nookies.get(context).userEmail || ''),
-    staleTime: Infinity,
-    cacheTime: Infinity,
-  })
+  try {
+    await queryClient.fetchQuery({
+      queryKey: userInfoKey(),
+      queryFn: userInfoFn({ initHeaders }, nookies.get(context).userEmail || ''),
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    })
 
-  return {
-    props: {
-      dehydratedState: dehydrate(queryClient),
-    },
+    return {
+      props: {
+        dehydratedState: dehydrate(queryClient),
+      },
+    }
+  } catch (e) {
+    return {
+      props: {},
+      redirect: { statusCode: 302, destination: '/login' },
+    }
   }
 })
 
