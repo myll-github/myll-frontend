ac110837830186a54f7d713d9e5763a99ee50225
jongdae <asdfg9377@naver.com>
2023-10-18 22:34:03 +0900

    feat: intro page

--- apps/web/src/pages/_app.page.tsx
+++ apps/web/src/pages/_app.page.tsx
@@ -36,14 +36,12 @@ const App = ({ Component, pageProps }: Props) => {
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
         />
-        <meta name="description" content="Description" />
-        <meta name="keywords" content="Keywords" />
-        <title>Next.js PWA </title>
+        <meta name="description" content="localtrip" />
+        <meta name="keywords" content="myll" />
+        <title>Myll</title>
 
         <link rel="manifest" href="/manifest.json" />
-        <link href="/icons/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
-        <link href="/icons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
-        <link rel="apple-touch-icon" href="/apple-icon.png" />
+        <link href="/icons/Illust_Intro.png" rel="icon" type="image/png" sizes="300x310" />
         <meta name="theme-color" content="#317EFB" />
       </Head>
       <QueryClientProvider client={queryClient}>
