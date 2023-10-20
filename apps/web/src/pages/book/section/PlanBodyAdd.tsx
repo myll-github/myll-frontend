a42f54d953f0dfed7e00f88dc92610b378e415b8
jongdae <asdfg9377@naver.com>
2023-10-20 21:52:14 +0900

    style: menulist style 적용

--- apps/web/src/pages/book/section/PlanBodyAdd.tsx
+++ apps/web/src/pages/book/section/PlanBodyAdd.tsx
@@ -8,16 +8,22 @@ const PlanBodyAdd = (props: PlanBodyAddProps) => {
   const { openModal } = props
   return (
     <div className="flex flex-col pl-30pxr pr-30pxr pt-20pxr mb-20pxr">
-      <div className="break-words HEADER-H2">초보 여행자님 어디로 떠나볼까요?</div>
-      <div className="break-words SUBTITLE-T6 mt-10pxr mb-20pxr">
-        카카오맵에서 여행 동선을 바로 확인하고 가고싶은 여행지를 등록해 여행계획을 짜보세요!
+      <div className="break-words HEADER-H2">
+        초보 여행자님 <br />
+        어디로 떠나볼까요?
       </div>
-      <div className="mb-20pxr">
-        <Alert type="warning" message="아직 여행계획이 없어요." closable={false} isVisible onVisibleChange={() => {}} />
+      <div className="break-words SUBTITLE-T6 mt-10pxr pb-20pxr">
+        카카오맵에서 여행 동선을 바로 확인하고 <br />
+        가고싶은 여행지를 등록해 여행계획을 짜보세요!
       </div>
-      <Button variant="large" type="button" onClick={openModal}>
-        여행일정 등록하기
-      </Button>
+      <div className="mt-20pxr mb-20pxr">
+        <Alert type="info" message="예정된 여행계획이 없어요." closable={false} isVisible onVisibleChange={() => {}} />
+      </div>
+      <div className="w-full flex justify-center">
+        <Button variant="block" type="button" onClick={openModal}>
+          여행일정 등록하기
+        </Button>
+      </div>
     </div>
   )
 }
--- packages/myll-ui/src/Components/MenuList/DefaultMenuItem.tsx
+++ packages/myll-ui/src/Components/MenuList/DefaultMenuItem.tsx
@@ -12,7 +12,9 @@ const DefaultMenuItem = ({ item, onClick, isSelectedButtonNeeded }: MenuItemProp
 
   return (
     <li
-      className="relative flex flex-row flex-shrink-0 w-full border-none h-70pxr px-20pxr py-10pxr"
+      className={`relative flex flex-row flex-shrink-0 w-full border-none h-70pxr px-20pxr py-10pxr ${
+        checkMap.has(item.id) && 'bg-SUB_BLUE_2'
+      }`}
       onClick={() => isSelectedButtonNeeded && ToggleCardStatusByClick(item)}
     >
       <CustomImage
@@ -23,14 +25,20 @@ const DefaultMenuItem = ({ item, onClick, isSelectedButtonNeeded }: MenuItemProp
       />
 
       <div className="relative flex flex-col w-full py-4pxr">
-        <button type="button" onClick={onClick} className="text-left SUBTITLE-T2 text-GRAY_80" disabled={!onClick}>
+        <button
+          type="button"
+          onClick={onClick}
+          style={{ width: 'calc(100% - 3.125rem)' }}
+          className="text-left SUBTITLE-T2 text-GRAY_80 w-full text-ellipsis overflow-hidden"
+          disabled={!onClick}
+        >
           {item.mainTitle}
         </button>
-        <span className="SUBTITLE-T8 text-GRAY_70">{item.subTitle}</span>
+        <span className="SUBTITLE-T8 text-GRAY_70 w-full text-ellipsis overflow-hidden">{item.subTitle}</span>
       </div>
 
       {isSelectedButtonNeeded && (
-        <div className="absolute right-0 transform -translate-y-1/2 top-1/2">
+        <div className="absolute right-0 transform -translate-y-1/2 top-1/2 pr-20pxr w-70pxr">
           <ButtonCondition isSelected={checkMap.has(item.id)} />
         </div>
       )}
